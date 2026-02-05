abcd01bcc906806cc7476db40e366e019b737f08
Research Dhamala <dhamala80130@gmail.com>
2026-02-05 22:47:58 +0545

    conneted with mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from "express";
 import authRoutes from "./src/routes/auth.routes.js";
 import "dotenv/config";
+import mongoose from "mongoose";
 const app = express();
 app.use("/file", authRoutes);
 const port = process.env.PORT;
-app.listen(port, () => {
-  console.log(`Server is started at ${port}`);
+app.listen(port, async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_CONNECTION_URL);
+    console.log(`Server is started at ${port}`);
+  } catch (error) {
+    console.log(`Error in starting server: ${error}`);
+  }
 });
