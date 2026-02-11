457c2410a3141c4f4019a222e718f910fcacf716
Research Dhamala <dhamala80130@gmail.com>
2026-02-11 23:44:33 +0545

    made file upload models

---
+++ backend/src/models/fileUpload.models.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+const fileUploadSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    file: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    sizeOfFile: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true },
+);
+const FileUploadModel = mongoose.model("fileUpload", fileUploadSchema);
+export default FileUploadModel;
